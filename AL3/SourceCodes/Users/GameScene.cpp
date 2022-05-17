2ad4abe29698e255709a0b6c099742d7dc5ac722
LE2A_04_OGAWA <k020g1408@g.neec.ac.jp>
2022-05-17 10:20:59 +0900

    ImGui対応の為色々変更、GameSceneの描画にsponzaを追加

--- AL3/SourceCodes/Users/GameScene.cpp
+++ AL3/SourceCodes/Users/GameScene.cpp
@@ -1,52 +1,61 @@
 #include "GameScene.h"
 #include "../3D/FbxLoader.h"
 #include "../3D/FbxObject3D.h"
-//#include "../Math/OgaJHelper.h"
+#include "../../imgui/ImguiControl.h"
+#include "../Math/OgaJHelper.h"
 
 GameScene::GameScene()
 {
-	modelA = Model::CreateFromObj("skydome");
-	modelB = Model::CreateFromObj("jihannki");
+	const float objA_Scale = 20.0f;
+	const float objB_Scale = 2.0f;
+
+	modelA = Model::CreateFromObj("monkey");
+	//modelB = Model::CreateFromObj("sponza");
+	modelB = Model::CreateFromObj("skydome");
 
 	objA = Object::Create(modelA);
 	objB = Object::Create(modelB);
 
-	objA->SetScale(XMFLOAT3(1.0f, 1.0f, 1.0f));
-	objB->SetScale(XMFLOAT3(5.0f, 5.0f, 5.0f));
+	objA->SetScale(XMFLOAT3(objA_Scale, objA_Scale, objA_Scale));
+	objB->SetScale(XMFLOAT3(objB_Scale, objB_Scale, objB_Scale));
 
 	light = Light::Create();
-	light->SetLightColor({ 1,1,1 });
+	light->SetLightColor(
+		{
+			ImguiControl::Imgui_lightColor_r,
+			ImguiControl::Imgui_lightColor_g,
+			ImguiControl::Imgui_lightColor_b
+		});
+	light->SetLightDir(
+		{
+			ImguiControl::Imgui_lightDir_x,
+			ImguiControl::Imgui_lightDir_y,
+			ImguiControl::Imgui_lightDir_z,
+		});
+
 	Object::SetLight(light);
 
-	Camera::SetEye(XMFLOAT3(0, 10, -50));
+	//Camera::SetEye(XMFLOAT3(0, 10, -50));
 
 	FbxObject3D::SetDevice(DirectXImportant::dev.Get());
 	FbxObject3D::CreateGraphicsPipeline();
 
-	fbxModel1 = FbxLoader::GetInstance()->LoadModelFromFile("boneTest");
+	fbxModel1 = FbxLoader::GetInstance()->LoadModelFromFile("newPlayer");
 
 	fbxObj1 = new FbxObject3D();
 	fbxObj1->Init();
 	fbxObj1->SetModel(fbxModel1);
+	fbxObj1->SetScale(XMFLOAT3(0.05, 0.05, 0.05));
+	fbxObj1->SetRotation(XMFLOAT3(-90, 0, 0));
 	fbxObj1->PlayAnimation();
-
-	Sprite::LoadTexture(0, L"Resources/hamurabyss.png");
-	GH1 = Sprite::Create(0, { WINDOW_WIDTH / 2,WINDOW_HEIGHT / 2 });
-	GH1->SetSize(XMFLOAT2{ 64, 64 });
-
-	p = { 0.0f,WINDOW_HEIGHT / 2 };
-	v = { 10.0f,-7.0f };
-	a = { 0.0f,0.0f };
-	f = { 0.0f,0.0f };
-	m = 50.0f;
-	t = 0.0f;
 }
 
 GameScene::~GameScene()
 {
+	//‘‚«–Y‚ê‚é‚©‚çƒ„ƒƒI
 	delete modelA;
-	delete modelB;
 	delete objA;
+	delete modelB;
 	delete objB;
 	delete fbxModel1;
 	delete fbxObj1;
@@ -54,15 +63,12 @@ GameScene::~GameScene()
 
 void GameScene::Init()
 {
-	objA->SetPosition(XMFLOAT3(0.0f, 0.0f, 0.0f));
-	objB->SetPosition(XMFLOAT3(-40.0f, 0.0f, 0.0f));
+	objA->SetPosition(XMFLOAT3(0.0f, 20.0f, 0.0f));
+	objA->SetRotation(XMFLOAT3(0, 180, 0));
 
-	objA->SetRotation(XMFLOAT3(0, 0, 0));
-	objB->SetRotation(XMFLOAT3(0, -90, 0));
-
-	Camera::SetEye({ 20,20.0f,-50.0f });
-	Camera::SetTarget({ 0.0f,5.0f,0.0f });
-	Camera::SetUp({ 0.0f,1.0f,0.0f });
+	//Camera::SetEye({ 20,20.0f,-50.0f });
+	Camera::SetTarget({ 0.0f,15.0f,0.0f });
+	//Camera::SetUp({ 0.0f,1.0f,0.0f });
 }
 
 void GameScene::Update()
@@ -70,51 +76,68 @@ void GameScene::Update()
 	objA->Update();
 	objB->Update();
 
+	light->SetLightColor(
+		{
+			ImguiControl::Imgui_lightColor_r,
+			ImguiControl::Imgui_lightColor_g,
+			ImguiControl::Imgui_lightColor_b
+		});
+	light->SetLightDir(
+		{
+			ImguiControl::Imgui_lightDir_x,
+			ImguiControl::Imgui_lightDir_y,
+			ImguiControl::Imgui_lightDir_z,
+		});
+	//Object::SetLight(light);
+
 	light->Update();
 
-	XMFLOAT3 rot = objA->GetRotation();
-	rot.y += 0.03f;
-	objA->SetRotation(rot);
+	XMFLOAT3 objApos = objA->GetPosition();
+	if (Input::isKey(DIK_W)) { objApos.z -= MAX_MOVE_SPEED; }
+	if (Input::isKey(DIK_S)) { objApos.z += MAX_MOVE_SPEED; }
+	if (Input::isKey(DIK_D)) { objApos.x -= MAX_MOVE_SPEED; }
+	if (Input::isKey(DIK_A)) { objApos.x += MAX_MOVE_SPEED; }
+	//objA->SetPosition(objApos);
 
-	XMFLOAT3 eye = Camera::GetEye();
-	if (Input::isKey(DIK_RIGHT)) { eye.x += 1.0f; }
-	if (Input::isKey(DIK_LEFT)) { eye.x -= 1.0f; }
-	Camera::SetEye(eye);
+	//XMFLOAT3 rot = objA->GetRotation();
+	//rot.y += 0.06f;
+	//objA->SetRotation(rot);
+
+	if (Input::isKey(DIK_RIGHT)) { cameraAngle.x += MAX_CAMERA_MOVE_SPEED; }
+	if (Input::isKey(DIK_LEFT)) { cameraAngle.x -= MAX_CAMERA_MOVE_SPEED; }
+
+	if (Input::isKey(DIK_UP)) {
+		if (cameraAngle.y < XM_PI / 2) { cameraAngle.y += MAX_CAMERA_MOVE_SPEED; }
+	}
+	if (Input::isKey(DIK_DOWN)) {
+		if (cameraAngle.y > -XM_PI / 5) { cameraAngle.y -= MAX_CAMERA_MOVE_SPEED; }
+	}
+
+	//OgaJHelper::ConvertToRadian(cameraAngle.x);
+	XMFLOAT3 cameraPos = { 0,0,0 };
+	float distance = MAX_DISTANCE;
+	float diff = 0;
+
+	cameraPos.y = sinf(cameraAngle.y) * MAX_DISTANCE;
+	//‹ßŠñ‚é‚©‚Ì”»’è
+	if (cameraPos.y < 0) {
+		diff = cameraPos.y; cameraPos.y = 0;
+	}
+
+	cameraPos.x = sinf(cameraAngle.x) * (distance + diff);
+	cameraPos.z = cosf(cameraAngle.x) * (distance + diff);
+
+	Camera::SetEye(cameraPos);
 
 	fbxObj1->Update();
-
-	p.x += v.x;
-	p.y += v.y;
-	v.x += a.x;
-	v.y += a.y;
-	a.x += f.x / m;
-	a.y += f.y / m;
-
-	p.y += -(t - 0.5 * pow(g, 2) * pow(t, 2));
-	t += 1.0f / 60.0f;
-
-	GH1->SetPosition(p);
-
-	if (Input::isKeyTrigger(DIK_R)) {
-		p = { 0.0f,WINDOW_HEIGHT / 2 };
-		v = { 10.0f,-5.0f };
-		a = { 0.0f,0.0f };
-		f = { 0.0f,0.0f };
-		m = 50.0f;
-		t = 0.0f;
-	}
 }
 
 void GameScene::Draw()
 {
 	Object::PreDraw(DirectXImportant::cmdList.Get());
-	objA->Draw();
-	objB->Draw();
+	//objA->Draw();
+	//objB->Draw();
 	Object::PostDraw();
 
 	fbxObj1->Draw(DirectXImportant::cmdList.Get());
-
-	Sprite::PreDraw(DirectXImportant::cmdList.Get());
-	GH1->Draw();
-	Sprite::PostDraw();
 }
