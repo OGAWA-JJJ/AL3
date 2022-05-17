f221f2a4c9f22264fb98b0e123c70e3743617f0a
LE2A_04_OGAWA <k020g1408@g.neec.ac.jp>
2022-05-17 10:19:31 +0900

    Setter,Getterを追加

--- AL3/SourceCodes/3D/FbxObject3D.h
+++ AL3/SourceCodes/3D/FbxObject3D.h
@@ -91,5 +91,11 @@ public:
 public:
 	void SetModel(FbxModel* model) { this->model = model; }
 	//Setter‚ðì‚êI
+	void SetScale(XMFLOAT3& scale) { this->scale = scale; }
+	void SetRotation(XMFLOAT3& rotation) { this->rotation = rotation; }
+	void SetPosition(XMFLOAT3& position) { this->position = position; }
+	const XMFLOAT3 &GetScale() { return scale; }
+	const XMFLOAT3& GetRotation() { return rotation; }
+	const XMFLOAT3& GetPosition() { return position; }
 };
 
