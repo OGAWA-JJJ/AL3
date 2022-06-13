bb6b61584b73a43ec46a29671b29cda4faa9e567
LE2A_04_OGAWA <k020g1408@g.neec.ac.jp>
2022-06-13 14:28:32 +0900

    新規追加系

---
+++ AL3/Resources/Shaders/ShadowMapPS.hlsl
@@ -0,0 +1,10 @@
+#include "ShadowMapShaderHeader.hlsli"
+
+Texture2D<float4> tex : register(t0);
+SamplerState smp : register(s0);
+
+float4 PSmain(VSOutput input) : SV_TARGET
+{
+    //float4 color = { 0.0f, 0.0f, 0.0f, 1.0f };
+    return tex.Sample(smp, input.uv);
+}
